a5202cdd7d9720d1cbe6c12cd3747820833b5e9f
Xochil73 <xochilsquaglia@yahoo.com>
2018-09-22 17:56:23 -0700

    commit of day one work... for now

---
+++ app.js
@@ -0,0 +1,77 @@
+"use strict";
+var pike = {
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookie: 6.3,
+    cookiesPerHour: [],
+    avgCustomers: function () {
+    for (var i = 0; i < 15; i++) {
+    var minCustomer = Math.ceil(this.minCustomer);
+    var maxCustomer = Math.floor(this.maxCustomer);
+    var customers: Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer; 
+    return customers
+      }
+      }
+      
+} 
+
+// var seatac = {
+//     minCustomer:3,
+//     maxCustomer: 24,
+//     avgCookie: 1.2,
+//     cookiesPerHour: [],
+//     avgCustomers: function (minCustomer, maxCustomer) {
+//     minCustomer = Math.ceil(minCustomer);
+//     maxCustomer = Math.floor(maxCustomer);
+//     var customer: Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer;
+//     return customers;   
+//     }
+//       }
+      
+// }
+
+// var seattleCenter = {
+//     minCustomer: 11,
+//     maxCustomer: 38,
+//     avgCookie:3.7,
+//     cookiesPerHour:[],
+//     avgCustomers: function (minCustomer, maxCustomer) {
+//     minCustomer = Math.ceil(minCustomer);
+//     maxCustomer = Math.floor(maxCustomer);
+//     var customers: Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer; 
+//     return customers; 
+//       };
+      
+      
+// };
+
+// var capHill = {
+//     minCustomer:20,
+//     maxCustomer:38,
+//     avgCookie: 2.3,
+//     cookiesPerHour:[],
+//     avgCustomers: function (minCustomer, maxCustomer) {
+//     minCustomer = Math.ceil(minCustomer);
+//     maxCustomer = Math.floor(maxCustomer);
+//     var customers: Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer; 
+//     return customers;
+
+//       }
+//       }
+      
+// }
+
+// var alki = {
+//     minCustomer:2,
+//     maxCustomer:16,
+//     avgCookie:4.6,
+//     cookiesPerHour:[],
+//     avgCustomers: function (minCustomer, maxCustomer) {
+//     minCustomer = Math.ceil(minCustomer);
+//     maxCustomer = Math.floor(maxCustomer);
+//     var customers:Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer; 
+//     return customers;
+//       }
+//       }
+      
+// }
